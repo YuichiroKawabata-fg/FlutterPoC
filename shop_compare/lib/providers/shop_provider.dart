6f67d0eca64637e39eda22504ef31cdf17ffc72f
YuichiroKawabata-fg <84459572+YuichiroKawabata-fg@users.noreply.github.com>
2025-06-05 15:50:51 +0900

    feat: integrate yahoo search

--- shop_compare/lib/providers/shop_provider.dart
+++ shop_compare/lib/providers/shop_provider.dart
@@ -1,14 +1,48 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../models/product.dart';
 
 class ShopProvider with ChangeNotifier {
   List<Product> _results = [];
   List<Product> get results => _results;
 
+  static const _yahooClientId =
+      'dj00aiZpPXJkMXJNYjZLTEQyMyZzPWNvbnN1bWVyc2VjcmV0Jng9NzE-';
+
   Future<void> search(String query) async {
-    await Future.delayed(const Duration(milliseconds: 300));
-    _results = _mockSearch(query);
+    final yahoo = await _searchYahoo(query);
+    final others = _mockSearch(query);
+    _results = [...yahoo, ...others];
     notifyListeners();
+  }
+
+  Future<List<Product>> _searchYahoo(String query) async {
+    final uri = Uri.https('shopping.yahooapis.jp',
+        '/ShoppingWebService/V3/itemSearch', {
+      'appid': _yahooClientId,
+      'query': query,
+      'hits': '10',
+    });
+    try {
+      final res = await http.get(uri);
+      if (res.statusCode == 200) {
+        final data = jsonDecode(res.body) as Map<String, dynamic>;
+        final hits = data['hits'] as List<dynamic>;
+        return hits.map((e) {
+          return Product(
+            shopName: 'Yahoo',
+            name: e['name'] ?? '',
+            price: (e['price'] as num?)?.toInt() ?? 0,
+            shipping: 0,
+            eta: '',
+          );
+        }).toList();
+      }
+    } catch (_) {
+      // ignore errors
+    }
+    return [];
   }
 
   List<Product> _mockSearch(String query) {
